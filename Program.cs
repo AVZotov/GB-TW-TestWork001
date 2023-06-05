c594c0208d11b37f8ba017500f40e3e6daf74f3b
Alexey_Zotov <Alexey.Zotov@ymail.com>
2023-06-05 22:26:24 +0300

    Support methods to get user array added

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿string[] GetArray()
+{
+    string[] userArray = new string[GetUint()];
+
+    for (int i = 0; i < userArray.Length; i++)
+    {
+        Console.Write($"Enter {i + 1} string: ");
+        userArray[i] = Console.ReadLine();
+    }
+
+    return userArray;
+}
+
+uint GetUint()
+{
+    uint userInput = 0;
+    Console.Write("Enter number of strings: ");
+
+    while ((uint.TryParse(Console.ReadLine(), out userInput)) == false)
+        Console.Write("Error!, Enter positive number: ");
+
+    return userInput;
+}
+
+void PrintArray(string[] array)
+{
+    foreach (var str in array)
+    {
+        Console.Write(str + " ");
+    }
+}
+
+string[] userArray = GetArray();
+PrintArray(userArray);
